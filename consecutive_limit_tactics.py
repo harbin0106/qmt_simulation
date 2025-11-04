6c51b9c36d883de74f27c7eaa4db1f0b626fb582
thomas <harbin0106@163.com>
2025-11-04 12:13:53 +0800

    Refine the code of trade_is_to_sell()

--- consecutive_limit_tactics.py
+++ consecutive_limit_tactics.py
@@ -178,10 +178,10 @@ def on_timer_simulate(contextInfo):
 
 def after_init(contextInfo):
 	print(f'after_init()')
-	account = get_trade_detail_data(T.accountid, T.accountid_type, 'account')
-	if len(account) == 0:
-		print(f'after_init(): Error! 账号{T.accountid} 未登录! 请检查!')
-		return
+	# account = get_trade_detail_data(T.accountid, T.accountid_type, 'account')
+	# if len(account) == 0:
+	# 	print(f'after_init(): Error! 账号{T.accountid} 未登录! 请检查!')
+	# 	return
 	# trade_query_info(contextInfo)
 	# trade_sell_stock(contextInfo, T.codes_all[8])
 	# trade_buy_stock(contextInfo, T.codes_all[0], 10000)
@@ -214,13 +214,13 @@ def trade_is_to_sell(contextInfo):
 	print(f'trade_is_to_sell(): {list(T.codes_to_sell["股票代码"].unique())}')
 	for code in T.codes_to_sell['股票代码']:
 		# 获取开盘价
-		market_data = contextInfo.get_market_data_ex(['open'], [code], period='1d', count=1, dividend_type='front', fill_data=True, subscribe=True)
+		market_data = contextInfo.get_market_data_ex(['open'], [code], period='1d', count=1, dividend_type='front', fill_data=False, subscribe=True)
 		open = market_data[code]['open'].iloc[0]
 		# 获取昨日收盘价
-		market_data_yesterday = contextInfo.get_market_data_ex(['close'], [code], period='1d', count=2, dividend_type='front', fill_data=True, subscribe=True)
+		market_data_yesterday = contextInfo.get_market_data_ex(['close'], [code], period='1d', count=2, dividend_type='front', fill_data=False, subscribe=True)
 		yesterday_close = market_data_yesterday[code]['close'].iloc[0]  # iloc[0]是昨天，iloc[1]是今天
 		# 获取当前的最新价格
-		market_data_current = contextInfo.get_market_data_ex(['lastPrice'], [code], period='tick', count=1, dividend_type='front', fill_data=True, subscribe=True)
+		market_data_current = contextInfo.get_market_data_ex(['lastPrice'], [code], period='tick', count=1, dividend_type='front', fill_data=False, subscribe=True)
 		current = market_data_current[code]['lastPrice'].iloc[0]
 		recommendation_date = str(T.codes_to_sell[T.codes_to_sell['股票代码'] == code]['指定日期T'].iloc[0])
 		current_date = timetag_to_datetime(contextInfo.get_bar_timetag(contextInfo.barpos), "%Y%m%d%H%M%S")[:8]
